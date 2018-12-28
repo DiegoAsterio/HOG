a5cea1253c4d3998a64b97f7184f207272cfa4bf
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-12-28 18:45:57 +0100

    Comentarios

--- src/descriptorHOG.py
+++ src/descriptorHOG.py
@@ -15,7 +15,9 @@ def gammaNormalization(img,c1=1,c2=0.5):
     @param c1 Factor multiplicativo en la normalización, por defecto 1.
     @param c2 Exponente de la normalización, por defecto 1/2 (compressing normalization).
     '''
+    # Restringe los valores de la imagen entre 0 y 1
     reduced = img/255.0
+    # Hace la correción de la luz
     corrected = np.power(reduced*c1,c2)
     return (corrected*255).astype(np.uint8)
 
@@ -31,13 +33,17 @@ def spatialOrientationBinning(gradients,tam_cel=3):
     @param gradients Matriz con los gradientes
     @param tam_cel Tamaño de la célula, por defecto 3.
     '''
+    # Obtiene el número de filas y columnas de la imagen
     rows = gradients.shape[0]
     cols = gradients.shape[1]
 
+    # Inicializa los histogramas
     histograms = []
 
+    # Divide la matriz en celdas y llama con cada una al cálculo de histogramas.
     for i in range(0,rows,tam_cel):
         for j in range(0,cols,tam_cel):
+            # Añade el histograma de la célula
             histograms.append(auxFunc.computeHistogram(gradients[i:i+tam_cel,j:j+tam_cel]))
 
     return histograms
