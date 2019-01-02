a7e67413b7a7838d8dc604f4ed4939d0c2c120de
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-02 13:30:24 +0100

    Ultima versión

--- src/descriptorHOG.py
+++ src/descriptorHOG.py
@@ -206,4 +206,5 @@ def obtainTrainData():
     img_descr = []
     for histo in histograms:
         img_descr.append(rhog(histo).reshape(-1))
-    return cv.ml.TrainData_create(np.array(img_descr),cv.ml.COL_SAMPLE,np.array(resp))
+    pdb.set_trace()
+    return cv.ml.TrainData_create(np.array(img_descr),cv.ml.ROW_SAMPLE,np.array(resp))
