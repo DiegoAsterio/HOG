ae40ab0341482eee05dc1aa21c6cba2585b35ed0
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-04 18:17:38 +0100

    Fallo

--- src/auxFunc.py
+++ src/auxFunc.py
@@ -149,8 +149,6 @@ def convexCombOfTwo(point, vpoints):
             coef1 = 1 - (point-vpoints[i-1])/tam
             coef2 = (point-vpoints[i-1])/tam
             return i-1, coef1, i, coef2
-        elif vpoints[i]==point:
-            return i-1, 0, i, 1
     return False
 
 def computeHistogram(subMag, subAng, num_cols, threeSixtyQ=False):
