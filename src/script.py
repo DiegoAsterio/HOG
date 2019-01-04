481b832d9f127e0940dd2eb9690bd0bd98316ece
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-04 18:13:44 +0100

    Cambios hasta ahora2

--- src/script.py
+++ src/script.py
@@ -11,19 +11,26 @@ svm = descriptorHOG.trainSVM(td)
 del td
 print("Cargandos las imagenes de test")
 img_pos, img_neg = af.loadTestImgs()
+img_pos = random.sample(img_pos,100)
+img_neg = random.sample(img_neg,100)
 img_pos_res = 0
 img_neg_res = 0
 predicciones = []
 print("Obteniendo las ventanas y descriptores de las imágenes pregunta")
+imagenes_positivas = []
 for img in img_pos:
     windows = random.sample(af.getAllWindows(img),20)
-    desc = descriptorHOG.obtainDescriptors(windows)
+    desc = descriptorHOG.obtainDescriptors(windows,True)
     img_pos_res+=len(windows)
+    contador=0
     for pred in svm.predict(desc)[1]:
         predicciones.append(pred[0])
+        if pred[0]==1:
+            imagenes_positivas.append(np.uint8(windows[contador]))
+        contador+=1
 for img in img_neg:
     windows = random.sample(af.getAllWindows(img),20)
-    desc = descriptorHOG.obtainDescriptors(windows)
+    desc = descriptorHOG.obtainDescriptors(windows,True)
     img_neg_res+=len(windows)
     for pred in svm.predict(desc)[1]:
         predicciones.append(pred[0])
@@ -41,12 +48,14 @@ npos=0
 nneg=0
 for i in range(len(predicciones)):
     if i<img_pos_res:
-        if predicciones[i]<1.5:
+        if predicciones[i]==1:
             npos+=1
     else:
-        if predicciones[i]>1.5:
+        if predicciones[i]==2:
             nneg+=1
 print("\n\n##################################################")
 print("Positivos: " + str(npos) + "/" + str(img_pos_res))
 print("Negativos: " + str(nneg) + "/" + str(img_neg_res))
 print("##################################################\n\n")
+
+af.pintaMI(imagenes_positivas)
