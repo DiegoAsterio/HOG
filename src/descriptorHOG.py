f2a78f2169d4a5a468de32b7d2d6210bf6cd2749
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-11 17:43:47 +0100

    Error en el formato de la impresión

--- src/descriptorHOG.py
+++ src/descriptorHOG.py
@@ -255,7 +255,7 @@ def obtainDescriptors(imgs,silent=False):
     #---- Print de información ----#
     if not silent:
         print("Calculando los descriptores de imagen")
-    contador=1
+    contador=2
     #---- Fin del print de información ----#
 
     # Normalizamos por bloques
@@ -263,7 +263,7 @@ def obtainDescriptors(imgs,silent=False):
     for histo in img_descr[1:]:
 
         #---- Print de información ----#
-        if not silent and (contador%100==0 or contador==len(imgs)-1 or contador==1):
+        if not silent and (contador%100==0 or contador==len(imgs) or contador==1):
             print("Calculando el descriptor final " + str(contador) + "/" + str(len(imgs)))
         contador+=1
         #---- Fin del print de información ----#
