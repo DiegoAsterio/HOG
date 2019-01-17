abef0f4ec8b2ea4737c927a6666abf583f07a0f2
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-17 16:26:04 +0100

    Test para las imagenes recortadas

---
+++ src/test_cropped.py
@@ -0,0 +1,35 @@
+import auxFunc as asf
+import descriptorHOG
+import cv2 as cv
+
+print("Cargando la SVM de fichero")
+svm = cv.ml.SVM_load("svm.txt")
+
+print("Cargando el test")
+img_pos, img_neg = af.loadTestImgs()
+
+descriptors = descriptorHOG.obtainDescriptors(img_pos+img_neg)
+
+predictions_raw = svm.predict(descriptors)[1]
+del svm
+predictions = [pred[0] for pred in predictions_raw]
+
+pred_pos = predictions[:len(img_pos)]
+pred_neg = predictions[len(img_pos):]
+
+pos_score = 0
+neg_score = 0
+
+for pred in pred_pos:
+    if pred==1:
+        pos_score+=1
+
+for pred in predictions_negative:
+    if pred==2:
+        neg_score+=1
+
+print("\n\n##################################################")
+print("Positivos: " + str(pos_score) + "/" + str(len(pred_pos)) + "===>" + str(100*pos_score/len(pred_pos)) + "%")
+print("Negativos: " + str(neg_score) + "/" + str(len(pred_neg)) + "===>" + str(100*neg_score/len(pred_neg)) + "%")
+print("Porcentaje de acierto total: " + str(100*(pos_score+neg_score)/(len(pred_pos)+len(pred_neg))))
+print("##################################################\n\n")
