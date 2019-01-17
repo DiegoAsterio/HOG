91b7bd21c6ac486d6de478e9d17475c2f5e316e6
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-17 23:31:40 +0100

    Utiliza la función de las estadísticas

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ exit()
 del td
 '''
 print("Cargando la SVM de fichero")
-svm = cv.ml.SVM_load("svm_sin_hardpositives.txt")
+svm = cv.ml.SVM_load("svm.txt")
 
 print("Cargando el test")
 
@@ -32,18 +32,4 @@ print("Predicciones para las imagenes negativas: ")
 print(pred_neg)
 print("##################################################\n\n")
 
-pos_score = 0
-neg_score = 0
-
-for ppred in pred_pos:
-    pos_score+=ppred
-
-for npred in pred_neg:
-    neg_score+=npred
-
-print("\n\n##################################################")
-print("Positivos: " + str(pos_score) + "/" + str(len(pred_pos)) + "===>" + str(100*pos_score/len(pred_pos)) + "%")
-print("Negativos: " + str(neg_score) + "/" + str(len(pred_neg)) + "===>" + str(100*neg_score/len(pred_neg)) + "%")
-print("Porcentaje de acierto total con la media por imagen: " + str(100*(pos_score+neg_score)/(len(pred_pos)+len(pred_neg))) + "%")
-print("Peatones totales detectados: " + str(positivos_correctos) + "/" + str(positivos_totales) + "===>" + str(100*positivos_correctos/positivos_totales) + "%")
-print("##################################################\n\n")
+af.printStats(pred_pos, pred_neg, positivos_correctos, positivos_totales)
